452c58871e4a758b549d759992b14d128e875cb2
Sean Kozen <sgkozen@gmail.com>
2023-02-13 16:24:56 +0900

    Handling errors

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,8 +1,31 @@
-function ErrorPage () {
-    return (
-        <h1>An error has occured.</h1>
-    );
+import PageContent from "../components/PageContent";
 
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+
+function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An error occurred!";
+  let message = "Something went wrong.";
+
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
 
 export default ErrorPage;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,25 +1,28 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, json } from "react-router-dom";
 
-import EventsList from '../components/EventsList';
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
   const events = data.events;
 
-  return (
-      <EventsList events={events}/>
-  );
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
 //Cannot use hooks in a loader
-export async function loader () {
-  const response = await fetch('http://localhost:8080/events');
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events111");
 
   if (!response.ok) {
-    throw { message: 'Could not fetch events.' };
+    /*throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    }); */
+    return json({ message: "Could not fetch events." }, {
+      status: 500,
+    });
   } else {
-    return response;    
+    return response;
   }
-} 
+}
