b514bb2c7738e37f783fa4fd5f57658618c47851
Sean Kozen <sgkozen@gmail.com>
2023-02-15 15:54:07 +0900

    Validation and Request refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import HomePage from "./pages/Home";
 import EventDetailPage, {
-  loader as eventDetailLoader,
+  loader as eventDetailLoader, action as deleteEventAction
 } from "./pages/EventDetails";
 import EditEventPage from "./pages/EditEvent";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
+import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
+import { action as manipulateEventAction } from "./components/EventForm";
 
 const router = createBrowserRouter([
   {
@@ -30,12 +31,13 @@ const router = createBrowserRouter([
               {
                 index: true,
                 element: <EventDetailPage />,
+                action: deleteEventAction,
               },
-              { path: "edit", element: <EditEventPage />},
+              { path: "edit", element: <EditEventPage />, action: manipulateEventAction },
             ], 
           },
 
-          { path: "new", element: <NewEventPage />, action: newEventAction  },
+          { path: "new", element: <NewEventPage />, action: manipulateEventAction },
         ],
       },
     ],
