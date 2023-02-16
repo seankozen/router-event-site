f1093de56396e4960de3f2c2ffe9958bbd05d9f9
Sean Kozen <sgkozen@gmail.com>
2023-02-16 15:11:43 +0900

    defer()

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,28 +1,44 @@
-import { useLoaderData, json } from "react-router-dom";
+import { Suspense } from "react";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const data = useLoaderData();
-  const events = data.events;
+  const { events } = useLoaderData();
 
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-//Cannot use hooks in a loader
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     /*throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
       status: 500,
     }); */
-    return json({ message: "Could not fetch events." }, {
-      status: 500,
-    });
+    return json(
+      { message: "Could not fetch events." },
+      {
+        status: 500,
+      }
+    );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,10 @@
+import PageContent from "../components/PageContent";
+
 function HomePage () {
     return (
-        <h1>Home Page</h1>
+        <PageContent title="Welcome!">
+            <p>Browse all of our amazing events!</p>
+        </PageContent>
     );
 }
 
